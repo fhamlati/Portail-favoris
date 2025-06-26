b834f9e56955358b86a971ed418eb64bd6bd8386
fatiha <fhamlati@csj.qc.ca>
2025-06-26 16:02:11 -0400

    update sidebar

--- Main.js
+++ Main.js
@@ -2,28 +2,84 @@
   let selectedRegion = "";
 
   const regionLinks = {
-    region1: [
+    R18: [
       "https://https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
       "https://region1.example.com/intranet",
       "https://region1.example.com/jurisevolution",
       "https://region1.example.com/plateforme",
       "https://region1.example.com/sharepoint"
     ],
-    region2: [
+    R80: [
       "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
       "https://region2.example.com/intranet",
       "https://region2.example.com/jurisevolution",
       "https://region2.example.com/plateforme",
       "https://region2.example.com/sharepoint"
     ],
-    region3: [
+    R81: [
       "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
       "https://region3.example.com/intranet",
       "https://region3.example.com/jurisevolution",
       "https://region3.example.com/plateforme",
       "https://region3.example.com/sharepoint"
     ],
-    region4: [
+    R82: [
+      "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
+      "https://region4.example.com/intranet",
+      "https://region4.example.com/jurisevolution",
+      "https://region4.example.com/plateforme",
+      "https://region4.example.com/sharepoint"
+    ],
+    R84: [
+      "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
+      "https://region4.example.com/intranet",
+      "https://region4.example.com/jurisevolution",
+      "https://region4.example.com/plateforme",
+      "https://region4.example.com/sharepoint"
+    ],
+      R85: [
+      "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
+      "https://region4.example.com/intranet",
+      "https://region4.example.com/jurisevolution",
+      "https://region4.example.com/plateforme",
+      "https://region4.example.com/sharepoint"
+    ],
+      R86: [
+      "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
+      "https://region4.example.com/intranet",
+      "https://region4.example.com/jurisevolution",
+      "https://region4.example.com/plateforme",
+      "https://region4.example.com/sharepoint"
+    ],
+      R87: [
+      "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
+      "https://region4.example.com/intranet",
+      "https://region4.example.com/jurisevolution",
+      "https://region4.example.com/plateforme",
+      "https://region4.example.com/sharepoint"
+    ],
+      R88: [
+      "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
+      "https://region4.example.com/intranet",
+      "https://region4.example.com/jurisevolution",
+      "https://region4.example.com/plateforme",
+      "https://region4.example.com/sharepoint"
+    ],
+      R89: [
+      "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
+      "https://region4.example.com/intranet",
+      "https://region4.example.com/jurisevolution",
+      "https://region4.example.com/plateforme",
+      "https://region4.example.com/sharepoint"
+    ],
+      R90: [
+      "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
+      "https://region4.example.com/intranet",
+      "https://region4.example.com/jurisevolution",
+      "https://region4.example.com/plateforme",
+      "https://region4.example.com/sharepoint"
+    ],
+      R95: [
       "https://www.bing.com/search?q=color%20css&FORM=ARPSEC&PC=ARPL&PTAG=30532142",
       "https://region4.example.com/intranet",
       "https://region4.example.com/jurisevolution",
